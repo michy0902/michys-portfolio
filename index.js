c99a40a0feddf97bc43295edfbb7912707568387
michy0902 <michelle.kolawole13@gmail.com>
2024-02-12 16:42:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Simulate a delay for demonstration purposes (remove this in a real scenario)
+    setTimeout(function() {
+      // Hide the preloader after the fade-out animation
+      document.getElementById('preloader').style.display = 'none';
+    }, 4000); // Adjust the delay as needed
+  });
