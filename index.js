129894853f300b56d740d9bf82a4e540d8013a80
michy0902 <michelle.kolawole13@gmail.com>
2024-02-14 22:12:02 +0100

    edited html css and js files

--- index.js
+++ index.js
@@ -5,23 +5,3 @@ document.addEventListener('DOMContentLoaded', function() {
       document.getElementById('preloader').style.display = 'none';
     }, 4000); // Adjust the delay as needed
   });
-
- /* document.addEventListener('DOMContentLoaded', function() {
-    // Get references to the link and the div
-    var home = document.getElementById('home');
-    var home-content = document.getElementById('home-content');
-
-    // Add a click event listener to the link
-    home.addEventListener('click', function(event) {
-        // Prevent the default link behavior (e.g., navigating to a new page)
-        event.preventDefault();
-
-        // Toggle the visibility of the div
-        if (home-content.style.display === 'none') {
-            home-content.style.display = 'block';
-        } else {
-            home-content.style.display = 'none';
-        }
-    });
-});
-*/
